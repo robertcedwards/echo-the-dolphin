acacbdb8d6291daf4476d9fdb3b48f14d2b92853
Robert C Edwards <139775+robertcedwards@users.noreply.github.com>
2024-03-28 12:39:59 -0500

    redirect method
    Added a redirect based on users input
    Redirects to the GIF maker with the ens/wallet address in the url

--- src/app/api/echo/route.ts
+++ src/app/api/echo/route.ts
@@ -4,8 +4,6 @@ import { getSSLHubRpcClient, Message } from "@farcaster/hub-nodejs";
 const HUB_URL = process.env["HUB_URL"] || "nemes.farcaster.xyz:2283";
 const hubClient = getSSLHubRpcClient(HUB_URL);
 
-const postUrl = `${process.env["HOST"]}/api/code`;
-
 export async function POST(req: NextRequest) {
   const {
     untrustedData: { inputText },
@@ -13,42 +11,13 @@ export async function POST(req: NextRequest) {
   } = await req.json();
   const frameMessage = Message.decode(Buffer.from(messageBytes, "hex"));
   const validateResult = await hubClient.validateMessage(frameMessage);
+
   if (validateResult.isOk() && validateResult.value.valid) {
-    const validMessage = validateResult.value.message;
+    // Assuming inputText is the variable you want to use in the redirect URL
+    const redirectUrl = `https://airstack-nft-gif-builder.vercel.app/?address=${encodeURIComponent(inputText)}`;
 
-    let urlBuffer = validMessage?.data?.frameActionBody?.url ?? [];
-    const urlString = Buffer.from(urlBuffer).toString("utf-8");
-    if (!urlString.startsWith(process.env["HOST"] ?? "")) {
-      return new NextResponse("Bad Request", { status: 400 });
-    }
-
-    const message = inputText ?? "";
-    const imageUrl = `${process.env["HOST"]}/api/images/echo?date=${Date.now()}&message=${message}`;
-    return new NextResponse(
-      `<!DOCTYPE html>
-      <html>
-        <head>
-          <title>Echo Says:</title>
-          <meta property="og:title" content="Echo Says:" />
-          <meta property="og:image" content="${imageUrl}" />
-          <meta name="fc:frame" content="vNext" />
-          <meta name="fc:frame:post_url" content="${postUrl}" />
-          <meta name="fc:frame:image" content="${imageUrl}" />
-          <meta name="fc:frame:button:1" content="See code" />
-          <meta name="fc:frame:button:1:action" content="post_redirect" />
-          <meta name="fc:frame:button:2" content="(coming soon)" />
-          <meta name="fc:frame:button:2:action" content="link" />
-          <meta name="fc:frame:button:2:target" content="https://github.com/horsefacts/echo-the-dolphin" />
-        </head>
-        <body/>
-      </html>`,
-      {
-        status: 200,
-        headers: {
-          "Content-Type": "text/html",
-        },
-      }
-    );
+    // Perform the redirect
+    return NextResponse.redirect(redirectUrl);
   } else {
     return new NextResponse("Unauthorized", { status: 401 });
   }
